effd42197209430a90fe275cf63b02e817990bf5
Rafael-Franca01 <rafaelfaraujo10@hotmail.com>
2025-06-06 21:49:48 -0300

    removing shadows

--- scripts/LitObject.gd
+++ scripts/LitObject.gd
@@ -2,54 +2,38 @@ extends MeshInstance3D
 
 @export_group("Material Properties")
 @export var material_albedo : Color = Color(0.8, 0.8, 0.8, 1.0)
-@export var shader_ambient : Color = Color(0.05, 0.05, 0.1, 1.0) # Alpha aqui não faz muito sentido
+@export var shader_ambient : Color = Color(0.05, 0.05, 0.1, 1.0)
 @export var material_roughness : float = 0.5:
-	set(value): 
+	set(value):
 		_material_roughness_internal = clamp(value, 0.0, 1.0)
 		if is_instance_valid(_cached_shader_material) and not Engine.is_editor_hint():
 			_cached_shader_material.set_shader_parameter("material_roughness_uniform", _material_roughness_internal)
-	get: 
+	get:
 		return _material_roughness_internal
+@export_range(0.0, 1.0, 0.001) var material_f0_reflectivity : float = 0.04
+@export_range(0.0, 1.0) var material_metallic : float = 0.0
+@export var material_emission_color : Color = Color(0.0, 0.0, 0.0, 1.0)
 
-@export_range(0.0, 1.0, 0.001) 
-var material_f0_reflectivity : float = 0.04
+@export_group("Materials & Rendering")
+@export var main_pbr_material: ShaderMaterial
+@export_flags_3d_render var visual_layers_override : int = 1
 
-@export_range(0.0, 1.0)
-var material_metallic : float = 0.0
-
-@export var material_emission_color : Color = Color(0.0, 0.0, 0.0, 1.0) # Alpha aqui não faz muito sentido
-
-@export_group("Materials")
-# O material PBR principal que este objeto usará.
-@export var main_pbr_material: ShaderMaterial 
-
-# O material de profundidade para este objeto específico.
-# NOTA: Para o shadow mapping automático que estamos construindo, o sistema usará o
-# 'depth_pass_material' definido no nó CustomLight, não este.
-# Este campo pode ser usado para outros propósitos ou se você quiser um controle manual.
-@export var object_specific_depth_material: ShaderMaterial 
+@export var object_specific_depth_material: ShaderMaterial
 
 var _material_roughness_internal : float = 0.5
-var _cached_shader_material: ShaderMaterial # Referência ao material instanciado para este objeto
+var _cached_shader_material: ShaderMaterial
 
 func _ready():
-	_material_roughness_internal = material_roughness 
-	
+	_material_roughness_internal = material_roughness
+	self.layers = visual_layers_override
+
 	if not is_instance_valid(main_pbr_material):
-		printerr("Objeto '%s': main_pbr_material não atribuído! O objeto não será renderizado corretamente." % name)
+		printerr("Objeto '%s': main_pbr_material não atribuído!" % name)
 		return
 
-	# Cria uma instância única do material PBR para este objeto, se você quiser que cada objeto
-	# tenha parâmetros únicos que não afetem outros objetos usando o mesmo recurso ShaderMaterial.
-	# Se você quiser que todos os objetos compartilhando o recurso ShaderMaterial mudem juntos,
-	# não duplique. Para parâmetros por objeto, duplicar é geralmente necessário.
 	_cached_shader_material = main_pbr_material.duplicate() as ShaderMaterial
-	
-	# Define o material duplicado (instância única) no objeto
-	self.material_override = _cached_shader_material # Usar material_override para consistência
-	# ou self.set_surface_override_material(0, _cached_shader_material) se você tiver múltiplas superfícies
+	self.material_override = _cached_shader_material
 
-	# Aplica os parâmetros exportados ao material instanciado
 	if is_instance_valid(_cached_shader_material):
 		_cached_shader_material.set_shader_parameter("albedo_color_uniform", material_albedo)
 		_cached_shader_material.set_shader_parameter("ambient_shader_color_uniform", Vector3(shader_ambient.r, shader_ambient.g, shader_ambient.b))
@@ -57,36 +41,13 @@ func _ready():
 		_cached_shader_material.set_shader_parameter("material_f0_scalar_uniform", material_f0_reflectivity)
 		_cached_shader_material.set_shader_parameter("material_metallic_uniform", material_metallic)
 		_cached_shader_material.set_shader_parameter("material_emission_color_uniform", Vector3(material_emission_color.r, material_emission_color.g, material_emission_color.b))
-		
-		# Registra este material instanciado no LightManager
+
 		if LightManager:
 			LightManager.register_lit_material(_cached_shader_material)
 		else:
 			printerr("Objeto '%s': LightManager (Autoload) não encontrado!" % name)
 	else:
 		printerr("Objeto '%s': Falha ao duplicar main_pbr_material." % name)
-
-
-# As funções abaixo são para troca manual e NÃO são usadas pelo sistema de shadow mapping automático.
-func use_object_specific_depth_material():
-	if is_instance_valid(object_specific_depth_material):
-		# Se estiver trocando, talvez desregistrar o PBR do LightManager?
-		if LightManager and is_instance_valid(_cached_shader_material):
-			LightManager.unregister_lit_material(_cached_shader_material)
-		self.material_override = object_specific_depth_material
-	else:
-		printerr("Objeto '%s': object_specific_depth_material não atribuído." % name)
-
-func use_main_pbr_material_again():
-	if is_instance_valid(_cached_shader_material): # _cached_shader_material é a instância do PBR
-		self.material_override = _cached_shader_material
-		# Re-registra se foi desregistrado
-		if LightManager:
-			LightManager.register_lit_material(_cached_shader_material)
-	elif is_instance_valid(main_pbr_material): # Fallback se _cached_shader_material se perdeu
-		_ready() # Tenta re-inicializar
-	else:
-		printerr("Objeto '%s': main_pbr_material não atribuído." % name)
 
 func _exit_tree():
 	if is_instance_valid(_cached_shader_material):
